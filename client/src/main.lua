58074ea448c8442c0b1a406ddda5862a3f7efc21
James Nguyen <james@jojojames.com>
2017-03-13 18:01:14 -0700

    Add some initial changes to remove some warnings in main
    - Started to fix some of the namespace warnings thrown using Lovetoys.
    - Fixed some variables that should be local and cleaned up some whitespace.
    - Moved some variables that belong to an 'instance' to a local M
    variable so that it can be used in more than one functin.
    
    Left some warnings alone as I don't understand enough of the code
    there to change it.

--- client/src/main.lua
+++ client/src/main.lua
@@ -1,33 +1,33 @@
+local love = require('love')
 local log = require('vendor.logger.log')
 
-lovetoys = require("lovetoys.lovetoys")
+local lovetoys = require("lovetoys.lovetoys")
 lovetoys.initialize({
   globals = true,
   debug = true
 })
 
-WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 600
-
-ONE_METER_IN_PIXELS = 64
-HORIZONTAL_GRAVITY = 0
-VERTICAL_GRAVITY = 9.81 * ONE_METER_IN_PIXELS
-
-BIRD_SPEED = 2
+-- Constants
+local WINDOW_WIDTH = 1000
+local WINDOW_HEIGHT = 600
+local ONE_METER_IN_PIXELS = 64
+local HORIZONTAL_GRAVITY = 0
+local VERTICAL_GRAVITY = 9.81 * ONE_METER_IN_PIXELS
+local BIRD_SPEED = 2
 
 -- components
 require("components/graphic/DrawableTwoPipe")
-local DrawableTwoPipe = Component.load({"DrawableTwoPipe"})
+local DrawableTwoPipe = lovetoys.Component.load({"DrawableTwoPipe"})
 require("components/identifier/PlayerBird")
-local PlayerBird = Component.load({"PlayerBird"})
+local PlayerBird = lovetoys.Component.load({"PlayerBird"})
 require("components/graphic/DrawableBird")
-local DrawableBird = Component.load({"DrawableBird"})
+local DrawableBird = lovetoys.Component.load({"DrawableBird"})
 require("components/common/Circle")
-local Circle = Component.load({"Circle"})
+local Circle = lovetoys.Component.load({"Circle"})
 require("components/common/Position")
-local Position = Component.load({"Position"})
+local Position = lovetoys.Component.load({"Position"})
 require("components/physic/Physic")
-local Physic = Component.load({"Physic"})
+local Physic = lovetoys.Component.load({"Physic"})
 
 -- systems
 local TwoPipeDrawSystem = require("systems/graphic/TwoPipeDrawSystem")
@@ -41,70 +41,71 @@ local BirdCameraEndSystem = require("systems/common/BirdCameraEndSystem")
 -- Events
 require("events/KeyPressed")
 
-function love.load(arg)
+local M = {}
+
+function love.load(_)
   love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {fullscreen=false, vsync=true, resizable=false})
   love.physics.setMeter(ONE_METER_IN_PIXELS)
-  world = love.physics.newWorld(HORIZONTAL_GRAVITY, VERTICAL_GRAVITY, true)
-  world:setCallbacks(beginContact, endContact)
+  M.world = love.physics.newWorld(HORIZONTAL_GRAVITY, VERTICAL_GRAVITY, true)
+  M.world:setCallbacks(beginContact, endContact)
 
-  engine = Engine()
-  eventmanager = EventManager()
+  M.engine = lovetoys.Engine()
+  M.eventmanager = lovetoys.EventManager()
 
-  mainkeysystem = MainKeySystem()
-  eventmanager:addListener("KeyPressed", mainkeysystem, mainkeysystem .fireEvent)
+  local mainkeysystem = MainKeySystem()
+  M.eventmanager:addListener("KeyPressed", mainkeysystem, mainkeysystem .fireEvent)
 
-  engine:addSystem(BirdCameraBeginSystem())
-  engine:addSystem(BirdDrawSystem())
-  engine:addSystem(TwoPipeDrawSystem())
-  engine:addSystem(BirdCameraEndSystem())
-  engine:addSystem(PhysicsPositionSyncSystem())
-  engine:addSystem(BirdBehaviorSystem(WINDOW_HEIGHT, BIRD_SPEED))
+  M.engine:addSystem(BirdCameraBeginSystem())
+  M.engine:addSystem(BirdDrawSystem())
+  M.engine:addSystem(TwoPipeDrawSystem())
+  M.engine:addSystem(BirdCameraEndSystem())
+  M.engine:addSystem(PhysicsPositionSyncSystem())
+  M.engine:addSystem(BirdBehaviorSystem(WINDOW_HEIGHT, BIRD_SPEED))
 
   --generate some pipes
   local PIPE_SPACING_BETWEEN_SETS = 200
   local x_last_pipe = 0
-  for i = 1, 20, 1 do
-    local two_pipe = Entity()
+  for _ = 1, 20, 1 do
+    local two_pipe = lovetoys.Entity()
     local x = x_last_pipe + PIPE_SPACING_BETWEEN_SETS
     local y = love.math.random(0, WINDOW_HEIGHT)
     two_pipe:add(DrawableTwoPipe(x, y))
-    engine:addEntity(two_pipe)
+    M.engine:addEntity(two_pipe)
     x_last_pipe = x
   end
 
   --generate the bird
-  local bird = Entity()
+  local bird = lovetoys.Entity()
   bird:add(PlayerBird())
   bird:add(DrawableBird())
   local x_bird, y_bird = 200, WINDOW_HEIGHT / 4
   local r_bird = 40
   bird:add(Position(x_bird, y_bird))
   bird:add(Circle(r_bird))
-  local body = love.physics.newBody(world, x_bird, y_bird, "dynamic")
+  local body = love.physics.newBody(M.world, x_bird, y_bird, "dynamic")
   local shape = love.physics.newCircleShape(r_bird)
   local fixture = love.physics.newFixture(body, shape, 0)
   body:setMass(1)
   bird:add(Physic(body, fixture, shape))
-  engine:addEntity(bird)
+  M.engine:addEntity(bird)
 
-
-  thread = love.thread.newThread("driver.lua")
+  local thread = love.thread.newThread("driver.lua")
   thread:start()
 end
 
 function love.draw()
-  engine:draw()
+  M.engine:draw()
 end
 
-function love.update(dt) 
-  engine:update(dt)
-  world:update(dt)
+function love.update(dt)
+  M.engine:update(dt)
+  M.world:update(dt)
 
   -- TODO: Figure out how to get the coordinates out.
   local update_luachan = love.thread.getChannel('update')
-  update_luachan:push({type = 'coordinates', payload = {x = 10, y = 20}})
+  -- update_luachan:push({type = 'coordinates', payload = {x = 10, y = 20}})
 end
 
 function love.keypressed(key, isrepeat)
-  eventmanager:fireEvent(KeyPressed(key, isrepeat))
+  M.eventmanager:fireEvent(KeyPressed(key, isrepeat))
 end
