efd5246b2aea0b422c30af96bcb6299e4ee878d2
James Nguyen <james@jojojames.com>
2017-03-19 16:30:34 -0700

    Remove extra space

--- client/src/components/graphic/drawable.lua
+++ client/src/components/graphic/drawable.lua
@@ -3,8 +3,8 @@ local drawable = Component.create("drawable")
 function drawable:initialize(image, r, sx, sy, ox, oy)
   self.image = image
   self.r = r
-  if sx then self.sx = sx  end
-  if sy then self.sy = sy  end
-  if ox then self.ox = ox  end
-  if oy then self.oy = oy  end
+  if sx then self.sx = sx end
+  if sy then self.sy = sy end
+  if ox then self.ox = ox end
+  if oy then self.oy = oy end
 end
