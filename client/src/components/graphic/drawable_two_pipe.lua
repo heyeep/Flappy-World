1cc30fb4563f81a8c755ad6949df68268c8c5aab
James Nguyen <james@jojojames.com>
2017-03-24 22:33:35 -0700

    Provide example of alternative function syntax

--- client/src/components/graphic/drawable_two_pipe.lua
+++ client/src/components/graphic/drawable_two_pipe.lua
@@ -2,7 +2,7 @@ _G.__DRAWABLE_TWO_PIPE = 'drawable_two_pipe'
 
 local drawable_two_pipe = Component.create(_G.__DRAWABLE_TWO_PIPE)
 
-function drawable_two_pipe:initialize(x, y)
+drawable_two_pipe.initialize = function(self, x, y)
   self.x = x
   self.y = y
 end
