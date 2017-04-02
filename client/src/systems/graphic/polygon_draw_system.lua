3b78c662e16ef73da5a842afc3f974b0ceb204f8
James Nguyen <james@jojojames.com>
2017-04-02 14:18:14 -0700

    Fix self in polygon_draw_system and clean up ~

--- client/src/systems/graphic/polygon_draw_system.lua
+++ client/src/systems/graphic/polygon_draw_system.lua
@@ -1,10 +1,11 @@
 local polygon_draw_system = class("polygon_draw_system", System)
 
-polygon_draw_system.draw = function()
+polygon_draw_system.draw = function(self)
   love.graphics.setColor(255, 255, 255)
-  for index, entity in pairs(self.targets) do
-    love.graphics.polygon("fill", entity:get("drawable_polygon").body:getWorldPoints(
-      entity:get("drawable_polygon").shape:getPoints()))
+  for _, entity in pairs(self.targets) do
+    love.graphics.polygon(
+      "fill", entity:get("drawable_polygon").body:getWorldPoints(
+        entity:get("drawable_polygon").shape:getPoints()))
   end
 end
 
