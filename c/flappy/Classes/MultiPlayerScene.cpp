e26f0da651510c6a1f896278e6c0f3bf11f677d0
Hiep Nguyen <hiepism@gmail.com>
2017-04-21 10:26:49 -0700

    Add singleplayerscene to build file

---
+++ c/flappy/Classes/MultiPlayerScene.cpp
@@ -0,0 +1,9 @@
+//
+//  MultiPlayerScene.cpp
+//  flappy
+//
+//  Created by Hiep Nguyen on 4/21/17.
+//
+//
+
+#include "MultiPlayerScene.hpp"
---
+++ c/flappy/Classes/MultiPlayerScene.h
@@ -0,0 +1,14 @@
+//
+//  MultiPlayerScene.hpp
+//  flappy
+//
+//  Created by Hiep Nguyen on 4/21/17.
+//
+//
+
+#ifndef MultiPlayerScene_hpp
+#define MultiPlayerScene_hpp
+
+#include <stdio.h>
+
+#endif /* MultiPlayerScene_hpp */
